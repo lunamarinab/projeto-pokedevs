d889e74ccd0fd60055dd9e2ced6da84bc00303b8
lunamarinab <lunamarina.b@gmail.com>
2024-10-13 17:12:02 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+const listaSelecaoPokedevs = document.querySelectorAll(".pokedev");
+
+listaSelecaoPokedevs.forEach(pokedev => {
+    pokedev.addEventListener("click", () => {
+        esconderCartaoPokedev();
+
+        const idPokedevSelecionado = mostrarCartaoPokedev(pokedev);
+
+        desativarPokedevNaListagem();
+
+        const pokedevSelecionadoNaListagem = document.getElementById(idPokedevSelecionado);
+        pokedevSelecionadoNaListagem.classList.add("ativo");
+    })
+})
+
+function desativarPokedevNaListagem() {
+    ativarPokedevNaListagem();
+}
+
+function ativarPokedevNaListagem() {
+    const pokedevAtivoNaListagem = document.querySelector(".ativo");
+    pokedevAtivoNaListagem.classList.remove("ativo");
+}
+
+function mostrarCartaoPokedev(pokedev) {
+    const idPokedevSelecionado = pokedev.attributes.id.value;
+    const idDoCartaoPokedevParaAbrir = "cartao-" + idPokedevSelecionado;
+    const cartaoPokedevParaAbrir = document.getElementById(idDoCartaoPokedevParaAbrir);
+    cartaoPokedevParaAbrir.classList.add("aberto");
+    return idPokedevSelecionado;
+}
+
+function esconderCartaoPokedev() {
+    const cartaoPokedevAberto = document.querySelector(".aberto");
+    cartaoPokedevAberto.classList.remove("aberto");
+}
